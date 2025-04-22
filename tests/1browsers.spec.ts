3d865ae9a5f02a3c06d978d2d267e2de57e2d1f5
mrreddy12 <mrreddygituser@gmail.com>
2025-04-22 11:01:28 +0530

    cc

--- tests/1browsers.spec.ts
+++ tests/1browsers.spec.ts
@@ -1,7 +1,7 @@
 import {test,expect} from "@playwright/test"
 
 test("browsers",async ({page})=>{
-    //goto//browser test
+    //goto//browser testnn
     await page.goto("https://mrreddyautomation.com/playground/");
   
 
