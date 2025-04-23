32cca733cb076550a146198bbeade7619f6cb6b6
mrreddy12 <mrreddygituser@gmail.com>
2025-04-23 11:06:47 +0530

    tr

--- tests/4button.spec.ts
+++ tests/4button.spec.ts
@@ -7,7 +7,7 @@ test("button",async ({page})=>{
 
     await page.locator("//span[text()='Buttons']").click();
    await page.waitForTimeout(5000);
-
+//
     const b= await page.locator("//input[@id='Login']").isVisible();
      console.log("visiable status : ",b);
      const b2= await page.locator("//input[@id='Login']").isEnabled();
