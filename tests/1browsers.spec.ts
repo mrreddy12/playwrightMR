934eee32c9bd6218d030d74a58fd6e3c73d95415
mrreddy12 <mrreddygituser@gmail.com>
2025-04-26 06:50:35 +0530

    c

--- tests/1browsers.spec.ts
+++ tests/1browsers.spec.ts
@@ -4,5 +4,5 @@ test("browsers",async ({page})=>{
     //goto//browser testnn
     await page.goto("https://mrreddyautomation.com/playground/");
   //reddy
-
+  await page.goto("https://mrreddyautomation.com/playground/");
 });
